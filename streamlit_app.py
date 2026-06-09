50bf0de03f6f7848e9abccc7099feac630cb8d4b
Miloš Ilić <104625382+milosilic2704@users.noreply.github.com>
2026-06-09 12:24:46 +0200

    Add nutrition information display for Smoothiefroot

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,6 +48,7 @@ if ingredients_list:
     #    session.sql(my_insert_stmt).collect()
     #    st.success('Your Smoothie is ordered!', icon="✅")
 
+# New Section to display smoothiefroot nutrition informaiton
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response.json())
-
+# st.text(smoothiefroot_response.json())
+st_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width = True)
